b1d89f6a02b8eebcd8560938bf60134f85b022bf
Kieran Labarrere <60007273+wawatusee@users.noreply.github.com>
2020-10-14 10:09:20 +0200

    Order des pieces avec la fonction getcomposedstyle

--- js/taquin.js
+++ js/taquin.js
@@ -1,30 +1,14 @@
 function taquin() {
-    console.log("lapin");
     var lesPieces = document.getElementsByClassName("piece");
+    //var ordrePiece = pieceOrdonne[0].style.order = "-1";
     var i = 0;
-    for (var piece in lesPieces) {
-        //le code ma boucle
+    for (var i = 0; i < lesPieces.length; i++) {
         var chaquePiece = lesPieces[i];
-        console.log(lesPieces[i]);
-        window.setComputedStyle(lesPieces[i]);
-        i += 1;
+        var sonStyle = getComputedStyle(chaquePiece);
+        console.log(chaquePiece);
+        console.log(sonStyle.order);
     };
 };
-
-/*function pieceCliquable(largueurTaquin, pieceInvisible, pieceAtester) {
-    var cliquable;
-    if (pieceAtester == (pieceInvisible - 1) || pieceAtester == (pieceInvisible + 1) || pieceAtester == (pieceInvisible + 4) || pieceAtester == (pieceInvisible - 4) && !(pieceAtester % largueurTaquin) && pieceAtester % largueurTaquin != 1) {
-        cliquable = "vrai";
-    } else cliquable = "faux";
-    return cliquable;
-}
-function pieceCliquable(pieceInvisible, pieceAtester, largueurTaquin = 4) {
-    var cliquable;
-    if (pieceAtester == (pieceInvisible - 1) && !(pieceAtester % largueurTaquin == 1) || pieceAtester == (pieceInvisible + 1) && !(pieceAtester % largueurTaquin) || pieceAtester == (pieceInvisible + 4) || pieceAtester == (pieceInvisible - 4)) {
-        cliquable = "vrai";
-    } else cliquable = "faux";
-    return cliquable;
-}*/
 function pieceCliquable(pieceInvisible, pieceAtester, largueurTaquin = 4) {
     var cliquable = (pieceAtester == (pieceInvisible - 1) && !(pieceInvisible % 4 == 1)) || (pieceAtester == (pieceInvisible + 1) && !(pieceInvisible % 4 == 0)) || (pieceAtester == (pieceInvisible + 4)) || (pieceAtester == (pieceInvisible - 4));
     /*Enlever la piece invisible des test*/
