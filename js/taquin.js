3f48f077bf240546615d0c5f07fdee88b1bf4792
wawatusee <web2020.kieran.labarrere@gmail.com>
2020-10-19 08:13:33 +0200

    Essai avec images

--- js/taquin.js
+++ js/taquin.js
@@ -1,57 +1,48 @@
 function taquin() {
     var lesPieces = document.getElementsByClassName("piece");
     /*Attribuer un ordre à un élément Flex :
-    var ordrePiece = pieceOrdonne[0].style.order = "-1";*/
-
     /*Trouver la piece invisible, stoquer son ordre dans une variable :*/
-    pieceInvisible=document.querySelector("#pieceInvisible");
+    pieceInvisible = document.querySelector("#pieceInvisible");
     console.log(pieceInvisible);
-    stylePieceInvisible=getComputedStyle(pieceInvisible);
-    stylePieceInvisible.order;
-    
-    var i = 0;
+    stylePieceInvisible = getComputedStyle(pieceInvisible);
     for (var i = 0; i < lesPieces.length; i++) {
         var chaquePiece = lesPieces[i];
-        chaquePiece.style.order=i+1;
+        chaquePiece.style.order = i + 1;
         var sonStyle = getComputedStyle(chaquePiece);
-        
-        chaquePiece.addEventListener("click",joue);
+
+        chaquePiece.addEventListener("click", joue);
         console.log(chaquePiece);
         console.log(sonStyle.order);
     };
 };
+
 function pieceCliquable(pieceInvisible, pieceAtester, largueurTaquin = 4) {
     pieceInvisible = Number(pieceInvisible);
     pieceAtester = Number(pieceAtester);
     //Cliquable est définit comme true quand l'ordre de la piece testéé est égale à l'ordre de la pièce invisible,-1 ou +1 ou -4 ou +4 sauf quand le reste de la division de l'orde de la piece invisible par la largeur du taquin est égal à 1 ou à 0
     var jouable = (pieceAtester == (pieceInvisible - 1) && (pieceInvisible % largueurTaquin != 1)) // Vérifie si déplaçable vers la gauche
-    || (pieceAtester == (pieceInvisible + 1) && (pieceInvisible % largueurTaquin != 0)) // Vérifie si déplaçable vers la droite
-    || (pieceAtester == (pieceInvisible + largueurTaquin)) // Vérifie si déplaçable vers le bas
-    || (pieceAtester == (pieceInvisible - largueurTaquin)); // Vérifie si déplaçable vers le haut
-    /*Enlever la piece invisible des test*/
+        ||
+        (pieceAtester == (pieceInvisible + 1) && (pieceInvisible % largueurTaquin != 0)) // Vérifie si déplaçable vers la droite
+        ||
+        (pieceAtester == (pieceInvisible + largueurTaquin)) // Vérifie si déplaçable vers le bas
+        ||
+        (pieceAtester == (pieceInvisible - largueurTaquin)); // Vérifie si déplaçable vers le haut
     return jouable;
 }
+
 function joue(evt) {
     //var nouvelleOrdrePieceCliquee=ordrePieceInvisible;
     var sonStyle = getComputedStyle(evt.target);
-    if(pieceCliquable(stylePieceInvisible.order,sonStyle.order)){
-    //pieceInvisible prend l'ordre de la piece cliquée, et piece cliquée prend l'ordre de la piece invisible
+    if (pieceCliquable(stylePieceInvisible.order, sonStyle.order)) {
+        // Echange l'order entre la pièce invisible et la pièce cliquée
+        let temporaryOrder = pieceInvisible.style.order;
+        pieceInvisible.style.order = sonStyle.order;
+        evt.target.style.order = temporaryOrder;
+        console.log('La pièce invisible qui est en position ' + stylePieceInvisible.order + ' prend la position de la piece cliquée ' + sonStyle.order);
+    } else {
+        console.log(evt);
 
-    // Echange l'order entre la pièce invisible et la pièce cliquée
-    let temporaryOrder = pieceInvisible.style.order;
-    pieceInvisible.style.order = sonStyle.order;
-    evt.target.style.order = temporaryOrder;
-
-    console.log('La pièce invisible qui est en position '+stylePieceInvisible.order+' prend la position de la piece cliquée '+ sonStyle.order);
-
-    //evt.target.style.order=nouvelleOrdrePieceCliquee;
-    //ordrePieceInvisible=14;
-    }else{
-    console.log(evt);
-
-    console.log(sonStyle.order);
-    console.log('Pièce cliquable: '+ stylePieceInvisible.order + " || " + sonStyle.order );
+        console.log(sonStyle.order);
+        console.log('Pièce cliquable: ' + stylePieceInvisible.order + " || " + sonStyle.order);
     };
 };
-
-
