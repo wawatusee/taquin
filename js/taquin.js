e7d45b90a4052f7d3752703c7dc28dc827bf5d5d
wawatusee <web2020.kieran.labarrere@gmail.com>
2021-04-06 16:39:48 +0200

    Have cleaned repositories

--- js/taquin.js
+++ js/taquin.js
@@ -1,24 +1,40 @@
 function taquin() {
+    /*Sélection de toutes les div identifiées pièces, dans un tableau "lesPieces", on range leurs réfèrences*/
     var lesPieces = document.getElementsByClassName("piece");
-    /*Attribuer un ordre à un élément Flex :
-    /*Trouver la piece invisible, stoquer son ordre dans une variable :*/
+    /*Trouver la piece invisible, */
     pieceInvisible = document.querySelector("#pieceInvisible");
+    /*stoquer son style dans une variable :*/
     stylePieceInvisible = getComputedStyle(pieceInvisible);
+    /*Tableau de positions autres */
+    var shuffleArray=[9,11,2,14,15,1,3,8,16,7,4,12,5,13,6,10];
+    /*Boucle sur les  */
     for (var i = 0; i < lesPieces.length; i++) {
-        console.log("Dans la boucle taquin"+" i est égal à"+ i+chaquePiece);
+        let largeurPiece=hauteurPiece=100;
         var chaquePiece = lesPieces[i];
-        chaquePiece.style.order = i + 1;
         var sonStyle = getComputedStyle(chaquePiece);
-        chaquePiece.addEventListener("click", joue);
         
-        console.log(sonStyle.order);
+        /*Placement de l'image de fond pour chaque piece */
+        chaquePiece.style.backgroundPositionX=`${-(i%4)*largeurPiece}px`;
+        chaquePiece.style.backgroundPositionY=`${-Math.floor(i/4)*largeurPiece}px`;
+        /*Fin du Placement du fond pour chaque piece */
+        /*chaquePiece.style.order = i+1;*/
+        chaquePiece.style.order =shuffleArray[i];
+        /*Mise en place des écouteurs sur chaque pièce */
+        chaquePiece.addEventListener("click", joue);
     };
 };
+/*Dans chessGame on a utilisé cette réplique :
+emplacement.style.backgroundPosition = `${pieces[figure][1]} ${couleurJouee}`;
+Ca marchait donc on va utiliser la même.
+Pour l'appliquer :
+Largeur de l'image (toujours la même)
+Hauteur de l'image largeur multipliée par ratio d'image
+ */
 
 function pieceCliquable(pieceInvisible, pieceAtester, largueurTaquin = 4) {
     pieceInvisible = Number(pieceInvisible);
     pieceAtester = Number(pieceAtester);
-    //Cliquable est définit comme true quand l'ordre de la piece testéé est égale à l'ordre de la pièce invisible,-1 ou +1 ou -4 ou +4 sauf quand le reste de la division de l'orde de la piece invisible par la largeur du taquin est égal à 1 ou à 0
+    //Cliquable est définit comme true quand l'ordre de la piece testée est égale à l'ordre de la pièce invisible,-1 ou +1 ou -4 ou +4 sauf quand le reste de la division de l'orde de la piece invisible par la largeur du taquin est égal à 1 ou à 0
     var jouable = (pieceAtester == (pieceInvisible - 1) && (pieceInvisible % largueurTaquin != 1)) // Vérifie si déplaçable vers la gauche
         ||
         (pieceAtester == (pieceInvisible + 1) && (pieceInvisible % largueurTaquin != 0)) // Vérifie si déplaçable vers la droite
@@ -41,26 +57,33 @@ function joue(evt) {
     } else {
         console.log(evt);
         console.log(sonStyle.order);
-        console.log('Pièce cliquable: ' + stylePieceInvisible.order + " || " + sonStyle.order);
+        console.log('Cliquabilité: la pièce invisible est en place ' + stylePieceInvisible.order + " || Et la tienne en " + sonStyle.order);
     };
 };
-let pieces = ["Piece1", "Piece2", "Piece3", "Piece4", "Piece5", "Piece6"];
 
-function randomPieces(tableauPieces) {
-    let availablePieces = Array(...tableauPieces);
-    let randomOrderPieces = [];
-        // Crée une variable contenant la longueur initiale de `availablePieces` pour éviter qu'elle ne soit réevaluée à chaque tour de boucle
-        let initialLength = availablePieces.length;
+function shuffleTaquin(array){
+    var lesPieces = document.getElementsByClassName("piece");
+    console.log(lesPieces+"Lapin");
+    for (var i = 0; i < lesPieces.length; i++) {
+        var chaquePiece = lesPieces[i];
+        var sonStyle = getComputedStyle(chaquePiece);
+        var nouvelOrdre=array[i];
+        chaquePiece.style.order = Number(nouvelOrdre);
+        console.log(`Ordre tiré du tableau shuffleTaquin ${nouvelOrdre}`);
+        console.log(sonStyle.order);
+        console.log(chaquePiece);
+    };
 
-    for (let i = 0; i < initialLength; i++) {
-                        // Choisi un index pour une pièce restante dans `availablePieces`
-                        let randomIndex = Math.floor(Math.random() * availablePieces.length);
-
-                        // Fait sauter une pièce à l'index précisé par `randomIndex` et renvoie un array contenant cette pièce
-                        let deletedPieceArray = availablePieces.splice(randomIndex, 1);
-
-            randomOrderPieces.push(...deletedPieceArray);
-        }
-
-    return randomOrderPieces;
 }
+//shuffleTaquin(shuffleArray);
+function taquinAfficheOrder() {
+    var lesPieces = document.getElementsByClassName("piece");
+    stylePieceInvisible = getComputedStyle(pieceInvisible);
+    for (var i = 0; i < lesPieces.length; i++) {
+        var chaquePiece = lesPieces[i];
+        var sonStyle = getComputedStyle(chaquePiece);
+        console.log(sonStyle.order);
+    };
+
+};
+taquinAfficheOrder();
