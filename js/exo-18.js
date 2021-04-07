b2b7fc52e4ddfcedab6eb2d15b30fcf6f9a5c7d2
wawatusee <web2020.kieran.labarrere@gmail.com>
2021-04-07 14:08:50 +0200

    Mise à jour fichier après fichier avec shuffle pour éviter conflits avec shuffleTaquin

--- js/exo-18.js
+++
@@ -1,7 +0,0 @@
-Exo 1 :
-let anniversaire=new Date(1969,08,02);
-anniversaire;
-Renvoie:
-Tue Sep 02 1969 00:00:00 GMT+0100 (heure d’été d’Europe centrale);
-Exo 2 :
-
--- js/taquin.js
+++ js/taquin.js
@@ -5,36 +5,29 @@ function taquin() {
     pieceInvisible = document.querySelector("#pieceInvisible");
     /*stoquer son style dans une variable :*/
     stylePieceInvisible = getComputedStyle(pieceInvisible);
-/*Boucle sur les  */
+    /*Tableau de positions autres */
+    var shuffleArray=[9,11,2,14,15,1,3,8,16,7,4,12,5,13,6,10];
+    /*Boucle sur les  */
     for (var i = 0; i < lesPieces.length; i++) {
         let largeurPiece=hauteurPiece=100;
         var chaquePiece = lesPieces[i];
         var sonStyle = getComputedStyle(chaquePiece);
-        chaquePiece.style.order = i+1;
+        
         /*Placement de l'image de fond pour chaque piece */
         chaquePiece.style.backgroundPositionX=`${-(i%4)*largeurPiece}px`;
         chaquePiece.style.backgroundPositionY=`${-Math.floor(i/4)*largeurPiece}px`;
         /*Fin du Placement du fond pour chaque piece */
+        /*chaquePiece.style.order = i+1;*/
+        chaquePiece.style.order =shuffleArray[i];
         /*Mise en place des écouteurs sur chaque pièce */
         chaquePiece.addEventListener("click", joue);
     };
 };
-/*Dans chessGame on a utilisé cette réplique :
-emplacement.style.backgroundPosition = `${pieces[figure][1]} ${couleurJouee}`;
-Ca marchait donc on va utiliser la même.
-Pour l'appliquer :
-Largeur de l'image (toujours la même)
-Hauteur de l'image largeur multipliée par ratio d'image
-
- */
-function shufflePieces(piecesToShuffle){
-    /*Ici le code de Nanard */
-}
 
 function pieceCliquable(pieceInvisible, pieceAtester, largueurTaquin = 4) {
     pieceInvisible = Number(pieceInvisible);
     pieceAtester = Number(pieceAtester);
-    //Cliquable est définit comme true quand l'ordre de la piece testéé est égale à l'ordre de la pièce invisible,-1 ou +1 ou -4 ou +4 sauf quand le reste de la division de l'orde de la piece invisible par la largeur du taquin est égal à 1 ou à 0
+    //Cliquable est définit comme true quand l'ordre de la piece testée est égale à l'ordre de la pièce invisible,-1 ou +1 ou -4 ou +4 sauf quand le reste de la division de l'orde de la piece invisible par la largeur du taquin est égal à 1 ou à 0
     var jouable = (pieceAtester == (pieceInvisible - 1) && (pieceInvisible % largueurTaquin != 1)) // Vérifie si déplaçable vers la gauche
         ||
         (pieceAtester == (pieceInvisible + 1) && (pieceInvisible % largueurTaquin != 0)) // Vérifie si déplaçable vers la droite
@@ -57,6 +50,17 @@ function joue(evt) {
     } else {
         console.log(evt);
         console.log(sonStyle.order);
-        console.log('Cliquabilité: la pièce invisible est en place ' + stylePieceInvisible.order + " || Et la tienne en" + sonStyle.order);
+        console.log('Cliquabilité: la pièce invisible est en place ' + stylePieceInvisible.order + " || Et la tienne en " + sonStyle.order);
     };
 };
+function taquinAfficheOrder() {
+    var lesPieces = document.getElementsByClassName("piece");
+    stylePieceInvisible = getComputedStyle(pieceInvisible);
+    for (var i = 0; i < lesPieces.length; i++) {
+        var chaquePiece = lesPieces[i];
+        var sonStyle = getComputedStyle(chaquePiece);
+        console.log(sonStyle.order);
+    };
+
+};
+//taquinAfficheOrder();
