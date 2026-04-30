b4112770103ec8f77e68bef55f38119259e231b5
Kieran Labarrere <web2020.kieran.labarrere@gmail.com>
2026-05-01 00:30:27 +0200

    en cours de debug

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 require_once "config.php";
-$jsonImageTaquin=json_decode(file_get_contents("js/image-taquin.json"));
-$nomImage=$jsonImageTaquin->image_taquin;
-$urlImage=$dirImages.$nomImage;
-if (isset($_GET["nbrPieces"])){
-    $nbrPiecesPerLine=sqrt($_GET["nbrPieces"]);
-}else{
-    $nbrPiecesPerLine=$jsonImageTaquin->nbrPiecesPerLine;
+$jsonImageTaquin = json_decode(file_get_contents("js/image-taquin.json"));
+$nomImage = $jsonImageTaquin->image_taquin;
+$urlImage = $dirImages . $nomImage;
+if (isset($_GET["nbrPieces"])) {
+    $nbrPiecesPerLine = sqrt($_GET["nbrPieces"]);
+} else {
+    $nbrPiecesPerLine = $jsonImageTaquin->nbrPiecesPerLine;
 }
-$largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
+$largeurPiece = round($sizeTaquin / $nbrPiecesPerLine);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,17 +23,23 @@ $largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
     <link href="zoombox/zoombox.css" rel="stylesheet" type="text/css" media="screen" />
     <title>Taquin</title>
     <?php
-    $dimensionsImage=getimagesize($urlImage);
-    $largeurImage=$dimensionsImage[0];
-    $hauteurImage=$dimensionsImage[1];
-    $ratioImage=$hauteurImage/$largeurImage;
+    $dimensionsImage = @getimagesize($urlImage);
+    $largeurImage = ($dimensionsImage && $dimensionsImage[0] > 0) ? $dimensionsImage[0] : 400;
+    $hauteurImage = ($dimensionsImage && $dimensionsImage[1] > 0) ? $dimensionsImage[1] : 400;
+    $ratioImage = $hauteurImage / $largeurImage;
     ?>
-    <style>:root{
-        --image-taquin: url('<?php echo "../".$urlImage ?>');
-        --ratioImage: <?php echo $ratioImage ?>;
-        --nbrPiecesPerLine: <?php echo $nbrPiecesPerLine ?>
-    }</style>
+    <style>
+        :root {
+            --image-taquin: url('<?php echo "../" . $urlImage ?>');
+            --ratioImage:
+                <?php echo $ratioImage ?>
+            ;
+            --nbrPiecesPerLine:
+                <?php echo $nbrPiecesPerLine ?>
+        }
+    </style>
 </head>
+
 <body>
     <div class="c1">
 
@@ -40,8 +47,9 @@ $largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
             <h1><?php echo $titrePage ?></h1>
             <a href="admin/admin.php" class="admin-link" target="_self">
                 <svg viewBox="0 0 16 16">
-                    <circle cx="8" cy="8" r="2.5"/>
-                    <path d="M8 1v2M8 13v2M1 8h2M13 8h2M3.05 3.05l1.41 1.41M11.54 11.54l1.41 1.41M3.05 12.95l1.41-1.41M11.54 4.46l1.41-1.41"/>
+                    <circle cx="8" cy="8" r="2.5" />
+                    <path
+                        d="M8 1v2M8 13v2M1 8h2M13 8h2M3.05 3.05l1.41 1.41M11.54 11.54l1.41 1.41M3.05 12.95l1.41-1.41M11.54 4.46l1.41-1.41" />
                 </svg>
                 Admin
             </a>
@@ -55,12 +63,13 @@ $largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
             <div class="controls">
                 <a class="ctrl-btn" href="images/<?php echo $nomImage; ?>" rel="zoombox[galerie]" title="Voir l'image">
                     <svg viewBox="0 0 16 16">
-                        <circle cx="8" cy="8" r="6"/>
-                        <line x1="8" y1="5" x2="8" y2="8"/>
-                        <circle cx="8" cy="10.5" r="0.75" fill="<?php echo 'rgb(190,119,119)' ?>" stroke="none"/>
+                        <circle cx="8" cy="8" r="6" />
+                        <line x1="8" y1="5" x2="8" y2="8" />
+                        <circle cx="8" cy="10.5" r="0.75" fill="<?php echo 'rgb(190,119,119)' ?>" stroke="none" />
                     </svg>
                 </a>
-                <button class="ctrl-btn" id="numeroButton" onclick="displayPiecesNumber();" title="Afficher les numéros">1</button>
+                <button class="ctrl-btn" id="numeroButton" onclick="displayPiecesNumber();"
+                    title="Afficher les numéros">1</button>
 
                 <div class="ctrl-spacer"></div>
                 <div class="ctrl-separator"></div>
@@ -71,7 +80,7 @@ $largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
                             <?php
                             $nbrPiecesTaquin = $nbrPiecesPerLine * $nbrPiecesPerLine;
                             $optionsNbrPieces = [9, 16, 25];
-                            foreach($optionsNbrPieces as $option){
+                            foreach ($optionsNbrPieces as $option) {
                                 $selected = ($option == $nbrPiecesTaquin) ? 'selected' : '';
                                 echo "<option value=\"$option\" $selected>$option</option>";
                             }
@@ -85,4 +94,5 @@ $largeurPiece=round($sizeTaquin/$nbrPiecesPerLine);
     </div>
     <script type="text/javascript" src="js/taquin.js"></script>
 </body>
+
 </html>
